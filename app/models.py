2dfee98093422330a9583a37a765e569012b3462
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-10-02 20:21:01 +0300

    defined the event model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Event(db.Model):
+    event_id = db.Column(db.Integer, primary_key=True)
+    organizer_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    poster = db.Column(db.Image)
+    name = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    capacity = db.Column(db.Integer, nullable=False)
