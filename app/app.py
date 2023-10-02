1e4db44a6d188d3ed4e24bda1363ad9a03b9f9c6
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-10-02 19:30:10 +0300

    created a flask application instance and configured sqlalchemy uri

--- app/app.py
+++ app/app.py
@@ -1,3 +1,8 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///occasional_sync.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
