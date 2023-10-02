287c2b4e71b6dcb35f0f978a65ced1ca536f41d9
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-10-02 19:38:03 +0300

    imported db and created a migrate instance

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_cors import CORS
+from models import db
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///occasional_sync.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+migrate = Migrate(app, db)
 CORS(app)
