c20e3480339f696c8b243c9b8b14aa05acecbefc
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-10-03 23:46:46 +0300

    installed packages

--- app/app.py
+++ app/app.py
@@ -1,17 +1,34 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db
+from flask_restful import Api, Resource, reqparse
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length
+from email_validator import validate_email, EmailNotValidError
+from datetime import datetime
+from models import db, User, Event, EventNotification
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'
+
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+jwt = JWTManager(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///occasional_sync.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.json.compact = False
 
 migrate = Migrate(app, db)
 CORS(app)
 
 db.init_app(app)
+api = Api(app)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
